7a2e6f7ce8343cd66a106e1e8e6975f9a2af5a05
heeheee <heeheee@likelion.org>
2017-05-11 11:49:55 +0000

    heroku init

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ root "home#index"
   post 'home/new'
   post 'home/edit'
   get 'home/update/:post_id' => 'home#update'
-  get'home/read/:post_id' => 'home#read'
-  get'home/delete/:post_id' => 'home#delete'
+  get 'home/read/:post_id' => 'home#read'
+  get 'home/delete/:post_id' => 'home#delete'
